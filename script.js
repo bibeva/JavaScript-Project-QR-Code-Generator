9b9dc9ac5e7fa7d281b37d0ce085d3cc7cf921a4
Bibek Shakya <bibekneva@gmail.com>
2024-05-08 00:28:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let imageBox = document.querySelector('.imageBox');
-let qrImage = document.querySelector('#qrImage');
-let qrText = document.querySelector('#qrText');
+const qrText = document.querySelector('#qrText');
+const imageBox = document.querySelector('.imageBox');
+const qrImage = document.querySelector('#qrImage');
+const btn = document.querySelector('#generateButton');
 
-function generateQR() {
+const generateQR = () => {
   if (qrText.value.length > 0) {
     qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
     imageBox.classList.add('showImage');
@@ -12,3 +13,5 @@ function generateQR() {
     qrText.classList.add('error');
   }
 }
+
+btn.addEventListener('click', generateQR);
